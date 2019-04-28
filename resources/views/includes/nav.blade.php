efa9f2d7937e62deea00613616f1378355d1904c
HebaKassem <hebakassem@yahoo.com>
2019-04-28 15:36:34 +0200

    edited api and general model

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -18,7 +18,7 @@
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-item nav-link active" href="./">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-item nav-link" href="../users/create">Sign up</a>
+        <a class="nav-item nav-link" href="../users/create">Add New User</a>
         <a class="nav-item nav-link" href="../users/login">Login</a>
         
       </div>
